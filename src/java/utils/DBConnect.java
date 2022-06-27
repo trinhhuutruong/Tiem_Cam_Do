25f9f5367db5bd853b74ec2d66b74df6f1e88c9a
younga0204 <anhttse150289@fpt.edu.vn>
2022-06-27 22:34:49 +0700

    ....

--- src/java/utils/DBConnect.java
+++ src/java/utils/DBConnect.java
@@ -20,12 +20,9 @@ public class DBConnect {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
             String sql = "jdbc:sqlserver://localhost:1433;databaseName=Q_ANT";
-
-<<<<<<< HEAD
-            Connection con = DriverManager.getConnection(sql, "sa", "12345");
-=======
+            
             Connection con = DriverManager.getConnection(sql, "sa", "1234567890");
->>>>>>> 7229f114bcf8dbcc0fa7d8c4004e3cdc7f2b9a2e
+
             return con;
         } catch (SQLException e){
             e.printStackTrace();
